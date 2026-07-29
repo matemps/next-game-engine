1abecaabee8ef804f8fe3324b6ff5f1b2c74ad74
matemps <matemps@outlook.com>
2026-07-29 19:32:30 -0400

    jump

--- scratch/src/Player.cpp
+++ scratch/src/Player.cpp
@@ -1,3 +1,5 @@
+#include <cmath>
+
 #include "Player.h"
 
 
@@ -29,18 +31,52 @@ Player::~Player()
     delete m_PlayerCamera;
 }
 
+float Player::CalculateJumpHeight()
+{
+    switch (m_PlayerState)
+    {
+        case Crouching:
+            return CROUCHING_JUMP_HEIGHT;
+        case Standing:
+            return (m_IsCrouched)
+                ? STANDING_JUMP_CROUCH_HEIGHT
+                : STANDING_JUMP_HEIGHT;
+        case Walking:
+            return (m_IsCrouched)
+                ? WALKING_JUMP_CROUCH_HEIGHT
+                : WALKING_JUMP_HEIGHT;
+    }
+}
+
+void Player::Jump()
+{
+    float jumpHeight = CalculateJumpHeight();
+    b3Vec3 velocity = b3Body_GetLinearVelocity(m_Body);
+
+    velocity.y = sqrt(2 * GRAVITY * jumpHeight);
+
+    b3Body_SetLinearVelocity(m_Body, velocity);
+}
+
 void Player::Crouch()
 {
     m_IsCrouched = !m_IsCrouched;
 
     m_EyeLevel = m_IsCrouched ? EYE_LEVEL_CROUCH : EYE_LEVEL;
-    m_MovementSpeed = m_IsCrouched ? MOVEMENT_SPEED_CROUCH : MOVEMENT_SPEED;
+    m_MovementSpeed = m_IsCrouched ? MOVEMENT_SPEED_CROUCH : MOVEMENT_SPEED_WALK;
 
     b3BoxHull hull = m_IsCrouched
         ? b3MakeBoxHull(HALF_WIDTH, HALF_HEIGHT_CROUCH, HALF_DEPTH)
         : b3MakeBoxHull(HALF_WIDTH, HALF_HEIGHT, HALF_DEPTH);
 
     b3Shape_SetHull(m_Shape, &hull.base);
+
+    b3Vec3 velocity = b3Body_GetLinearVelocity(m_Body);
+    m_PlayerState = m_IsCrouched
+        ? Crouching
+        : (velocity.x == 0.0f && velocity.z == 0.0f)
+            ? Standing
+            : Walking;
 }
 
 bool Player::IsGrounded()
@@ -69,7 +105,7 @@ void Player::SyncCameraToBody()
     Vector3 center = ToVector3(b3Body_GetPosition(m_Body));
     Vector3 feet = center - Vector3(0.0f, halfHeight, 0.0f);
 
-    m_PlayerCamera->SetPosition(feet + Vector3(0, static_cast<float>(m_EyeLevel), 0));
+    m_PlayerCamera->SetPosition(feet + Vector3(0.0f, m_EyeLevel, 0.0f));
 }
 
 void Player::Update(Keyboard::State kbState, float dt)
@@ -87,7 +123,7 @@ void Player::Update(Keyboard::State kbState, float dt)
     if (kbState.D) { /* move right */ }
     if (kbState.A) { /* move left */ }
 
-    if (kbState.Space && IsGrounded()) { /* jump */ }
+    if (kbState.Space && IsGrounded()) { Jump(); }
 
     SyncCameraToBody();
 }
--- scratch/src/Player.h
+++ scratch/src/Player.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <cmath>
-
 #include <d3d11_1.h>
 #include <dxgi1_6.h>
 #include "Keyboard.h"
@@ -28,10 +26,20 @@ class Player
         inline Camera* GetPlayerCamera() { return m_PlayerCamera; }
 
     private:
+        float CalculateJumpHeight();
+        void Jump();
         void Crouch();
         bool IsGrounded();
         void SyncCameraToBody();
 
+    private:
+        enum PlayerState
+        {
+            Crouching,
+            Standing,
+            Walking
+        };
+    
     private:
         // collision hull dimensions
         static constexpr float HULL_WIDTH = 32.0f;
@@ -44,12 +52,25 @@ class Player
         static constexpr float HALF_HEIGHT_CROUCH = HULL_HEIGHT_CROUCH * 0.5f;
 
         // eye position
-        static constexpr int EYE_LEVEL = 64;
-        static constexpr int EYE_LEVEL_CROUCH = 28;
+        static constexpr float EYE_LEVEL = 64.0f;
+        static constexpr float EYE_LEVEL_CROUCH = 28.0f;
 
         // movement speed
-        static constexpr float MOVEMENT_SPEED = 150.0f;
+        static constexpr float MOVEMENT_SPEED_WALK = 150.0f;
         static constexpr float MOVEMENT_SPEED_CROUCH = 63.3f;
+
+        // jump height
+
+        // crouching
+        static constexpr float CROUCHING_JUMP_HEIGHT = 21.0f;
+        
+        // standing
+        static constexpr float STANDING_JUMP_HEIGHT = 20.0f;
+        static constexpr float STANDING_JUMP_CROUCH_HEIGHT = 56.0f;
+
+        // walking
+        static constexpr float WALKING_JUMP_HEIGHT = 20.0f;
+        static constexpr float WALKING_JUMP_CROUCH_HEIGHT = 56.0f;
 
     private:
         b3WorldId m_PhysicsWorld;
@@ -57,10 +78,12 @@ class Player
         b3ShapeId m_Shape;
 
         Camera* m_PlayerCamera = new Camera();
-        
-        int m_EyeLevel = EYE_LEVEL;
 
-        float m_MovementSpeed = 150.0f;
+        PlayerState m_PlayerState = Standing;
+
+        float m_EyeLevel = EYE_LEVEL;
+
+        float m_MovementSpeed = MOVEMENT_SPEED_WALK;
 
         bool m_IsCrouched = false;
 };
